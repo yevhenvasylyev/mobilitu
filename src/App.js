f4d0ca585b370a75f4578f12905dd18aa9dc6edf
vasevgen <vasevgen@gmail.com>
2020-11-03 22:18:25 +0200

    added Optimize tag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Google tag is added
+          Google and Optimize tags are added
         </p>
         <a
           className="App-link"
